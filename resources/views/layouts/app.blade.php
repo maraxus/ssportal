9f2cdd563a43170507ef62f5252457cd52255329
Rodrigo <abanaunderline@hotmail.com>
2018-01-15 09:09:11 -0200

    changed branding text

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'SMSPortal') }}
+                        SMS Portal
                     </a>
                 </div>
 
@@ -44,7 +44,7 @@
                         <!-- Authentication Links -->
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Cadastrar</a></li>
+                            <!-- <li><a href="{{ route('register') }}">Cadastrar</a></li> -->
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
